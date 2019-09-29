cdd51d6574552c2a3332907ffd10edcf1edbbca5
donqu <itsol>
2019-09-30 00:18:53 +0700

    update

---
+++ constants/constans.js
@@ -0,0 +1,9 @@
+module.exports = {
+    User_Agent_Value: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36',
+    URL_HOME: 'http://192.168.8.1/html/home.html',
+    URL_SES_TOK_INFO: 'http://192.168.8.1/api/webserver/SesTokInfo',
+    URL_CHALLENGE_LOGIN: 'http://192.168.8.1/api/user/challenge_login',
+    URL_AUTHEN_LOGIN: 'http://192.168.8.1/api/user/authentication_login',
+    URL_DATA_SWITCH: 'http://192.168.8.1/api/dialup/mobile-dataswitch',
+    PWS: 'admin123'
+}
